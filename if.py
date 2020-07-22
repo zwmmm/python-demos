02e1ec24404ce62c0bb912d0ea7b4ec3fcce10ab
wayne <zhengwei@yunxi.tv>
2020-07-22 17:42:10 +0800

    01

---
+++ if.py
@@ -0,0 +1,7 @@
+username = input('请输入账号：')
+password = input('请输入密码：')
+
+if username == 'admin' and password == '123456':
+  print('成功')
+else:
+  print('失败')
---
+++ var.py
@@ -0,0 +1,4 @@
+a = int(input('a = '))
+b = int(input('b = '))
+
+print('%d + %d = %d' % (a, b, a + b))
